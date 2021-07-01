448808118b9e138cf08df16441f1d3c7e8bbca4d
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-07-01 08:02:03 -0400

    things

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+setup(
+    name="web",
+    author="me",
+    version="222222.2",
+    packages=["src"],
+    python_requires=">=3.9.0",
+    install_requires=requirements
+)
