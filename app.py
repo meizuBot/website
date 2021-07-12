4bc24b7a6d69b5c2ccfa8221c0fe1b1c9edf8161
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-07-11 21:50:50 -0400

    remove old index

--- app.py
+++ app.py
@@ -13,33 +13,15 @@ bot_name = "Walrus"
 
 async def index(request: Request) -> Response:
     return templates.TemplateResponse(
-        "bootstrap_index.html", context={"request": request, "name": bot_name}
+        "index.html", context={"request": request, "name": bot_name}
     )
 
 
 async def not_found(request, exc):
     return templates.TemplateResponse("404.html", context={"request": request})
 
-async def test(request: Request) -> Response:
-    return templates.TemplateResponse(
-        "bootstrap.html", context={"request": request, "name": "oogogoag"}
-    )
-
-async def test2(request):
-    return templates.TemplateResponse(
-        "newindex.html", context={"request": request}
-    )
-
-async def why(r):
-    item = r.path_params["endpoint"]
-    return Response(str(await r.app.fetch(item)))
-
-
-
 routes = [
     Route("/", endpoint=index),
-    Route("/navbar", endpoint=test),
-    Route("/test/{endpoint:path}", endpoint=why),
     Mount("/static", StaticFiles(directory="web/static")),
 ]
 
